8fbc3c3aba59deb9c01d1831a99962a3cc321c3d
Mark Wright <markwright785@gmail.com>
2017-05-14 17:00:57 +0100

    button not moving

--- assets/js/color.js
+++ assets/js/color.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 var randomColor = "000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
 var randomColor1 = "000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
 var randomColor2 = "000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
