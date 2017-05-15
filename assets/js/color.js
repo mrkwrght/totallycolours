abacbf389edb4bbe59e65b8c7c207494d7280aed
Mark Wright <markwright785@gmail.com>
2017-05-15 17:32:26 +0100

    v2.0

---
+++ assets/js/color.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.outline-color' ).each(function(index) {
+    var color = "000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+    var targetBox = document.getElementsByClassName("color")[index];
+    targetBox.style.backgroundColor = "#" + color;
+    var targetDiv = document.getElementsByClassName("colorlabel")[index];
+    targetDiv.innerHTML = "#" + color;
+  });
+});
